1613d79f12ec4f88fb0d04f92a47ecd0401f3612
lesiak <lesiak@outlook.com>
2019-05-15 14:13:26 +0200

    [ORMXML] Added scaffold

---
+++ springboot-hibernate-orm-xml/src/main/java/com/example/sandbox/springboothibernateormxml/SpringbootHibernateOrmXmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.sandbox.springboothibernateormxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHibernateOrmXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHibernateOrmXmlApplication.class, args);
+	}
+
+}
