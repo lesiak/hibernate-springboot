9a8465ddf57a716df125dee8d62024466f84643b
lesiak <lesiak@outlook.com>
2019-05-30 07:04:30 +0200

    [H2Console] Added generated project

---
+++ h2-customization-springboot/src/main/java/com/example/h2customizationspringboot/H2CustomizationSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.h2customizationspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2CustomizationSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2CustomizationSpringbootApplication.class, args);
+	}
+
+}
