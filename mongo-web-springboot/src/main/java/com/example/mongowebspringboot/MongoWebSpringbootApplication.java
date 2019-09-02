c38f64e766b12aeaea8946fd49a24eeebb16d6c7
lesiak <lesiak@outlook.com>
2019-09-02 09:57:42 +0200

    [MongoWeb] Added starter

---
+++ mongo-web-springboot/src/main/java/com/example/mongowebspringboot/MongoWebSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongowebspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoWebSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoWebSpringbootApplication.class, args);
+	}
+
+}
---
+++ mongo-web-springboot/src/test/java/com/example/mongowebspringboot/MongoWebSpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.mongowebspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MongoWebSpringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
