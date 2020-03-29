e9d3dfd78a1ee020ba6097e613cb8c1a67952d7a
lesiak <lesiak@outlook.com>
2020-03-29 23:49:36 +0200

    [JPA] Pojo n+1 with result transformer

--- hibernate-springboot/src/test/java/com/example/sandbox/hibernatespringboot/repo/pojonplus1/PojoNPlusOneTest.java
+++ hibernate-springboot/src/test/java/com/example/sandbox/hibernatespringboot/repo/pojonplus1/PojoNPlusOneTest.java
@@ -1,16 +1,24 @@
 package com.example.sandbox.hibernatespringboot.repo.pojonplus1;
 
+import com.example.sandbox.hibernatespringboot.model.pojonplus1.Pair;
 import com.example.sandbox.hibernatespringboot.model.pojonplus1.SimpleEntity;
+import org.hibernate.transform.Transformers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import java.util.List;
 
 @SpringBootTest
 public class PojoNPlusOneTest {
 
     @Autowired
     private SimpleEntityRepository simpleEntityRepository;
+
+    @Autowired
+    private EntityManager entityManager;
 
     @BeforeEach
     public void setup() {
@@ -27,5 +35,14 @@ public class PojoNPlusOneTest {
     @Test
     public void testGetRecords() {
         simpleEntityRepository.getRecords();
+    }
+
+    @Test
+    public void aaa() {
+        List<Pair<SimpleEntity, Boolean>> postDTOs = entityManager.createQuery(
+    "select m as first, false as second from SimpleEntity m")
+                .unwrap(org.hibernate.query.Query.class)
+                .setResultTransformer(Transformers.aliasToBean(Pair.class))
+                .getResultList();
     }
 }
