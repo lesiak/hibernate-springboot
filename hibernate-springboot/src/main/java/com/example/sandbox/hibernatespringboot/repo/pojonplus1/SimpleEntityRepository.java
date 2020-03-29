b6a7753ae442275ae6a40b8a0981af4bb7ef7eb5
lesiak <lesiak@outlook.com>
2020-03-29 23:25:16 +0200

    [JPA] Added strange n+1 behaviour

--- hibernate-springboot/src/main/java/com/example/sandbox/hibernatespringboot/repo/pojonplus1/SimpleEntityRepository.java
+++ hibernate-springboot/src/main/java/com/example/sandbox/hibernatespringboot/repo/pojonplus1/SimpleEntityRepository.java
@@ -1,7 +1,15 @@
 package com.example.sandbox.hibernatespringboot.repo.pojonplus1;
 
+import com.example.sandbox.hibernatespringboot.model.pojonplus1.Pair;
 import com.example.sandbox.hibernatespringboot.model.pojonplus1.SimpleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SimpleEntityRepository extends JpaRepository<SimpleEntity, Long> {
+
+    @Query("SELECT new com.example.sandbox.hibernatespringboot.model.pojonplus1.Pair(m, false) FROM SimpleEntity m")
+    List<Pair<SimpleEntity, Boolean>> getRecords();
+
 }
--- hibernate-springboot/src/test/java/com/example/sandbox/hibernatespringboot/repo/pojonplus1/TestPojoQuery.java
+++ hibernate-springboot/src/test/java/com/example/sandbox/hibernatespringboot/repo/pojonplus1/TestPojoQuery.java
@@ -1,10 +1,15 @@
 package com.example.sandbox.hibernatespringboot.repo.pojonplus1;
 
+import com.example.sandbox.hibernatespringboot.model.pojonplus1.SimpleEntity;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,8 +18,23 @@ public class TestPojoQuery {
     @Autowired
     private SimpleEntityRepository simpleEntityRepository;
 
+    @Autowired
+    private EntityManager entityManager;
+
+    @Before
+    public void setup() {
+        simpleEntityRepository.deleteAll();
+        simpleEntityRepository.save(new SimpleEntity(1L, "aaa"));
+        simpleEntityRepository.save(new SimpleEntity(2L, "bbb"));
+    }
+
     @Test
     public void testFindAll() {
         simpleEntityRepository.findAll();
+    }
+
+    @Test
+    public void testGetRecords() {
+        simpleEntityRepository.getRecords();
     }
 }
